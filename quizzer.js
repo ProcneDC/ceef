ea0611beb3e4bd89791f65b8bfae22401868ec2e
ProcneDC <procne@riseup.net>
2019-08-10 23:52:42 -0300

    Initial commit

---
+++ quizzer.js
@@ -0,0 +1,400 @@
+var questions = [
+[
+"¿Cuáles son los motores principales en la sentadilla?",
+"Recto femoral y glúteos",
+"Vastos y glúteos",
+"Recto femoral y vastos",
+"Vastos e isquiotibiales",
+1
+],
+[
+"¿Planos y ejes de la extensión en el sillón de cuádriceps?",
+"Plano sagital, eje latero-lateral",
+"Plano frontal, eje antero-posterior",
+"Plano horizontal, eje vertical",
+"Plano frontal y sagital",
+0
+],
+[
+"¿Hay trabajo en isometría?",
+"Trabajo mecánico estático",
+"Trabajo de resistencia",
+"Ninguna de las anteriores",
+"No hay trabajo mecánico, no hay recorrido",
+3
+],
+[
+"Ejemplo de flexión ósea",
+"Los huesos no resisten la flexión",
+"Sólo se da con el curl, en el radio y cúbito",
+"Hay flexión en todos los ejercicios",
+"Ninguno de los anteriores",
+2
+],
+[
+"¿De qué tipo de contracción muscular hablamos cuando se acercan los puntos de inserción?",
+"Excéntrica",
+"Auxotónica",
+"Concéntrica",
+"Ninguna de las anteriores",
+2
+],
+[
+"Funciones del dorsal ancho",
+"Extensión de brazos y depresión de hombros",
+"Abeducción de brazos",
+"Extensión de hombro, aducción de hombro y rotador externo escapular",
+"Ninguna de las anteriores",
+2
+],
+[
+"¿Plano en una dominada de agarre cerrado?",
+"Plano sagital",
+"Plano frontal",
+"Plano horizontal",
+"Plano sagital - frontal",
+0
+],
+[
+"¿Qué son las bandas o discos Z?",
+"Son las que liberan calcio en el ciclo activo",
+"Son los neurotransmisores de las unidades motoras de los músculos",
+"Están dentro de la miosina y actina del músculo",
+"Son los límites del sarcómero",
+3
+],
+[
+"¿Cuánto puede acortarse un sarcómero?",
+"Se acorta hasta que no queda espacio entre las actinas (banda h)",
+"Se acorta hasta que no queda espacio entre los sarcómeros",
+"Se acorta un 50%",
+"Se acorta al máximo cuando finaliza el ciclo activo",
+0
+],
+[
+"¿Qué es la acetilcolina?",
+"Es lo que provoca la acidosis metabólica por la acumulación de ácido láctico",
+"Es un aminoácido ramificado BCAA",
+"Es un neurotransmisor que produce la liberación de calcio para la contracción muscular",
+"Ninguna de las anteriores",
+2
+],
+[
+"¿Diferencia entre hipertrofia e hiperplasia?",
+"El primero es el aumento de la masa muscular y el segundo es la atrofia",
+"El primero aumenta el número de fibras, el segundo el de miofibrillas",
+"Todas son incorrectas",
+"El primero aumenta el número de miofibrillas, el segundo el de fibras",
+3
+],
+[
+"Ejercicio de tipo de contracción muscular isocinética",
+"Curl",
+"Sentadillas",
+"Press de banca",
+"Todas las anteriores",
+3
+],
+[
+"¿Qué es la tensión muscular?",
+"Trabajo / Tiempo",
+"Fuerza / Superficie",
+"Tensión x Superficie",
+"Ninguna de las anteriores",
+1
+],
+[
+"¿Qué es la potencia muscular?",
+"Trabajo / Tiempo",
+"Fuerza / Superficie",
+"Tensión x Superficie",
+"Ninguna de las anteriores",
+0
+],
+[
+"¿Qué es el Trabajo muscular?",
+"Trabajo / Tiempo",
+"Tensión x Superficie",
+"Peso x Recorrido",
+"Ninguna de las anteriores",
+2
+],
+[
+"¿Cómo son las fibras del tipo IIb?",
+"Rápidas, Grandes, Glucolíticas-anaerobias, Fatigables",
+"Lentas, Pequeñas, Oxidantes, Resistentes",
+"Intermedias",
+"Rápidas, Grandes, Glucolíticas-aerobias, Resistentes",
+0
+],
+[
+"¿Cómo son las fibras del tipo I?",
+"Rápidas, Grandes, Glucolíticas-anaerobias, Fatigables",
+"Intermedias",
+"Lentas, Pequeñas, Oxidantes, Resistentes",
+"Rápidas, Grandes, Glucolíticas-aerobias, Resistentes",
+2
+],
+[
+"¿De qué se trata la tabla de Yuri Verkhoshansky?",
+"Cálculo de 1RM a partir de la carga que conozco",
+"La relación entre la fuerza y la velocidad concéntrica en una contracción muscular aislada",
+"Coeficiente de volumen para mesociclos",
+"% de fuerza en RM",
+3
+],
+[
+"1RM = 0.033 x peso x RM + peso <br> ¿A quién pertenece esa fórmula?",
+"A. Hill",
+"Epley",
+"Yuri Verkhoshansky",
+"Dorian Yates",
+1
+],
+[
+"Músculos que cruzan la articulación del hombro",
+"Deltoides, biceps y triceps",
+"Deltoide medio",
+"Todas son correctas",
+"Supraespinoso, redondo, dorsal ancho, pectoral",
+2
+],
+[
+"¿Cuál <strong>no</strong> es uno de los Principios del entrenamiento?",
+"Nutrición, entrenamiento y descanso",
+"Especificidad",
+"Resistencia progresiva",
+"Alternancia de cargas",
+0
+],
+[
+"¿En qué se degradan los macronutrientes que nos aportan energía?",
+"Carbohidratos en amilasas, Proteinas en proteasas, Grasas en lipasas",
+"Carbohidratos en 4 Kcal/g, Proteinas en 4 Kcal/g, Grasas en 9 Kcal/g",
+"Carbohidratos en 2,7 g agua, Proteinas en 1 g agua, Grasas en 1 g agua ",
+"Carbohidratos en glucosa, Proteinas en aminoácidos, Grasas en ácidos grasos libres y glicerol",
+3
+],
+[
+"¿Cómo se calcula el Valor Calórico Total para deportistas?",
+"Entrenamiento = Dieta + planificación + energía + recuperación + anabolismo",
+"Carbohidratos: 4 Kcal/g, Proteinas: 4 Kcal/g, Grasas: 9 Kcal/g",
+"Carbohidratos: 60, Proteinas: 20, Grasas: 20 ",
+"Con el coeficiente k (entre 30 y 40 kcal/peso según se quiera mantener, bajar grasas o ganar masa magra)",
+3
+],
+[
+"¿Suplementos selectivos: aminoácidos?",
+"BCAA (ramificados), creatina monohidratada, hidrolizados de proteina, BCAA, Carnitina",
+"Totales, BCAA (ramificados), glutamina, taurina, arginina, lanina",
+"Glucosa, cafeína, bicarbonato de sodio",
+"Carnitina, CLA, Chitosan, Garnicinia cambogia",
+1
+],
+[
+"¿Suplementos selectivos: lipolíticos?",
+"Carnitina, CLA, Chitosan, Garnicinia cambogia",
+"Glucosa, cafeína, bicarbonato de sodio",
+"Creatina monohidratada, picolinato de cromo, inosina, HMB",
+"Aislados de proteina, concentrados de proteina (whey), hidrolizados de proteina (sin lactosa)",
+0
+],
+[
+"¿Cómo funciona el eje hormonal?",
+"Hipotálamo - Hipófisis - glándula suprarrenal - adrenalina",
+"Hipotálamo - Hipófisis - glándula suprarrenal - cortisol",
+"Todas son correctas",
+"Hipotálamo - Hipófisis - adenohipófisis provoca que testiculos/ovarios descarguen testosterona",
+2
+],
+[
+"Esteroides Anabólicos Androgénicos, ¿cómo funciona a nivel celular?",
+"Esteroide + hormona receptora entra en el núcleo, ARN mensajero sale al citoplasma hacia los ribosomas",
+"Esteroide + hormona receptora entra en el núcleo, ARN mensajero sale al citoplasma hacia las mitocondrias",
+"Esteroide + hormona receptora entra en el núcleo, ARN mensajero sale al citoplasma hacia el retículo plasmático liso",
+"Esteroide + hormona receptora entra en el núcleo, ARN mensajero sale hacia el torrente sanguíneo",
+0
+],
+[
+"¿De qué se trata la técnica Davis?",
+"Superserie de 1 ejercicio monoarticular seguido de un ejercicio poliarticular",
+"Series al fallo con alguna variante: excéntricas, forzadas, estática",
+"Series de 1 repetición y 10 segundos de descanso. Cantidad de series: al fallo",
+"10 series al fallo con un descanso que irá incrementándose de 30 segundos a 4.30 minutos",
+3
+],
+[
+"¿De qué se trata la técnica descanso pausa?",
+"Superserie de 1 ejercicio monoarticular seguido de un ejercicio poliarticular",
+"Series al fallo con alguna variante: excéntricas, forzadas, estática",
+"10 series al fallo con un descanso que irá incrementándose de 30 segundos a 4.30 minutos",
+"Series de 1 repetición y 10 segundos de descanso. Cantidad de series: al fallo",
+3
+],
+[
+"¿De qué se trata la técnica de preagotamiento?",
+"Series al fallo con alguna variante: excéntricas, forzadas, estática",
+"Superserie de 1 ejercicio monoarticular seguido de un ejercicio poliarticular",
+"10 series al fallo con un descanso que irá incrementándose de 30 segundos a 4.30 minutos",
+"Series de 1 repetición y 10 segundos de descanso. Cantidad de series: al fallos",
+1
+],
+];
+
+var questionTemplate = _.template(" \
+<div class='card question'><span class='question'><%= question %></span> \
+<ul class='options'> \
+<li> \
+<input type='radio' name='question[<%= index %>]' value='0' id='q<%= index %>o1'> \
+<label for='q<%= index %>o1'><%= a %></label> \
+</li> \
+<li> \
+<input type='radio' name='question[<%= index %>]' value='1' id='q<%= index %>o2'> \
+<label for='q<%= index %>o2'><%= b %></label> \
+</li> \
+<li> \
+<input type='radio' name='question[<%= index %>]' value='2' id='q<%= index %>o3'> \
+<label for='q<%= index %>o3'><%= c %></label> \
+</li> \
+<li> \
+<input type='radio' name='question[<%= index %>]' value='3' id='q<%= index %>o4'> \
+<label for='q<%= index %>o4'><%= d %></label> \
+</li> \
+</ul> \
+</div> \
+");
+
+var points,
+  pointsPerQuestion,
+  currentQuestion,
+  questionTimer,
+  timeForQuestion = 10, // seconds
+  timeLeftForQuestion;
+$(function() {
+  $('button.start').click(start);
+  $('.play_again button').click(restart);
+
+  function restart() {
+    points = 0;
+    pointsPerQuestion = 10;
+    currentQuestion = 0;
+    timeLeftForQuestion = timeForQuestion;
+    $('.finish.card').hide();
+    $('div.start').show();
+    $('.times_up').hide();
+
+    generateCards();
+    updateTime();
+    updatePoints();
+  }
+
+  function start() {
+    $('div.start').fadeOut(200, function() {
+      moveToNextQuestion();
+    });
+  }
+
+  function generateCards() {
+    $('.questions').html('');
+    for (var i = 0; i < questions.length; i++) {
+      var q = questions[i];
+      var html = questionTemplate({
+        question: q[0],
+        index: i,
+        a: q[1],
+        b: q[2],
+        c: q[3],
+        d: q[4],
+        e: q[6]
+      });
+      $('.questions').append(html);
+    };
+
+    $('.question.card input').change(optionSelected);
+  }
+
+  function moveToNextQuestion() {
+    currentQuestion += 1;
+    if (currentQuestion > 1) {
+      $('.question.card:nth-child(' + (currentQuestion - 1) + ')').hide();
+    }
+
+    showQuestionCardAtIndex(currentQuestion);
+    setupQuestionTimer();
+  }
+
+  function setupQuestionTimer() {
+    if (currentQuestion > 1) {
+      clearTimeout(questionTimer);
+    }
+    timeLeftForQuestion = timeForQuestion;
+    questionTimer = setTimeout(countdownTick, 1000);
+  }
+
+  function showQuestionCardAtIndex(index) { // staring at 1
+    var $card = $('.question.card:nth-child(' + index + ')').show();
+  }
+
+  function countdownTick() {
+    timeLeftForQuestion -= 1;
+    updateTime();
+    if (timeLeftForQuestion == 0) {
+      return finish();
+    }
+    questionTimer = setTimeout(countdownTick, 3000);
+  }
+
+  function updateTime() {
+    $('.countdown .time_left').html('Tiempo: ' + timeLeftForQuestion + 's');
+  }
+
+  function updatePoints() {
+    $('.points span.points').html(points + ' puntos');
+  }
+
+  function optionSelected() {
+    var selected = parseInt(this.value);
+    var correct = questions[currentQuestion - 1][5];
+
+    if (selected == correct) {
+      points += pointsPerQuestion;
+      updatePoints();
+      correctAnimation();
+    } else {
+      wrongAnimation();
+    }
+
+    if (currentQuestion == questions.length) {
+      clearTimeout(questionTimer);
+      return finish();
+    }
+    moveToNextQuestion();
+  }
+
+  function correctAnimation() {
+    animatePoints('right');
+  }
+
+  function wrongAnimation() {
+    animatePoints('wrong');
+  }
+
+  function animatePoints(cls) {
+    $('header .points').addClass('animate ' + cls);
+    setTimeout(function() {
+      $('header .points').removeClass('animate ' + cls);
+    }, 500);
+  }
+
+  function finish() {
+    if (timeLeftForQuestion == 0) {
+      $('.times_up').show();
+    }
+    $('p.final_points').html(points + '/300 puntos');
+    $('.question.card:visible').hide();
+    $('.finish.card').show();
+  }
+
+  restart();
+});
+Resources
